cadcc508bf297e566f4578a95edcfb3372ed8edb
Bryant Zepeda <redtition@homail.com>
2015-03-03 18:01:26 -0800

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,14 +1,14 @@
 /*
-	Zepeda, Bryant
-	Lab #2
-	Due Date: 3/3/2015
-	Description: To gain experience in character processing
+Zepeda, Bryant
+Lab #2
+Due Date: 3/3/2015
+Description: To gain experience in character processing
 */
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
 int main(void)
 {
-	char io; 
+	char io;
 	int word = 1, punc = 0;
 
 	while ((io = getchar()) != EOF)
@@ -23,10 +23,10 @@ int main(void)
 		if (io >= 33 && io <= 64)
 		{
 			if (io == 33 || io == 44 || io == 46 || io == 59 || io == 63)
-				io = ' ';
-			punc++;
+				punc++;
+			io = ' ';
 		}
-		putchar(io);	
+		putchar(io);
 	}
 	printf("There are %d words and %d punctuation marks.\n", word, punc);
 	system("pause");
